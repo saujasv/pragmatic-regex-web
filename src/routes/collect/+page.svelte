a355de75d4050f1d5a16b1a3da33983c3f61d1d4
Saujas <saujas1999@gmail.com>
2022-11-22 16:11:58 -0500

    initial collection interface

---
+++ src/routes/collect/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Collect from './Collect.svelte';
+</script>
+
+<svelte:head>
+    <title>Regex from Examples</title>
+    <meta name="description" content="Collect examples of examples used to communicate about regexes" />
+</svelte:head>
+  
+<main>
+<Collect user={"foo"}/>
+</main>
+
+<style>
+</style>
